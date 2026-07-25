b905c44809db29135cf48d666de064e8e147fb9f
Anthony Savchenko <anthony.savchenko@gmail.com>
2026-07-25 20:40:29 +0300

    Что-то забыл закомитить

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://antonsavchenko-try-astro.netlify.app/",
   integrations: [preact()],
+  i18n: {
+    locales: ["ru", "en"],
+    defaultLocale: "ru",
+  },
 });
